5ac2ba97675968101b10e3353da5521ecdd0a07c
Nat Arney <gysoran@gmail.com>
2022-02-11 17:01:39 -0600

    Cards now have limits on what they can look like

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,34 @@
 export class Card {
-    constructor(title=" ",type=" ",val=0,flavor=` `,art=" ") {
+    constructor(title="",type=0,val=0,flavor=``,art="") {
+        this.types = ["air", "earth", "fire", "water"];
+
         this.title = title;
-        this.type = type;
-        this.val = val;
+        this.type = ((type > -1 && type < types.length) ? 
+                        types[type] : types[0]);
+        this.val = (val > -1 && val < 16) ? val : 0;
         this.flavor = flavor;
-        this.art = art;
+        this.art = art; // TODO: Placeholder art
     }
 
     // Getter
     get card() {
-        return [this.title,this.type,this.val,this.flavor,this.art];
+        return [
+            this.title, 
+            this.type, 
+            this.val, 
+            this.flavor, 
+            this.art
+        ];
     }
 }
+
+/* 
+    Card
+    types   :   Array of strings
+    /////////////////////////////
+    title   :   Some string
+    type    :   String from types
+    val     :   Num from 0-15
+    flavor  :   Some long string
+    art     :   Some string (path to img)
+*/

cd5fbbe759b06624b11778a36317df3c89a42e02
Pouchrat <gysoran@gmail.com>
2022-02-23 21:07:49 -0600

    Moved index.js to root, fixed an error in Card.js

--- index.js
+++ index.js
@@ -16,12 +16,19 @@ http.createServer(function (req,res){
       res.end();
     });
     }
-    else if(req.url === '/script/Card.js') {
-       fs.readFile('src/script/Card.js',function(err,data) {
+    else if(req.url === '/script/main.js') {
+       fs.readFile('src/script/main.js',function(err,data) {
           res.writeHead(200,{"Content-Type": "text/javascript"});
           res.write(data);
           res.end();
        });
     }
+    else if(req.url === '/script/Card.js') {
+      fs.readFile('src/script/Card.js',function(err,data) {
+         res.writeHead(200,{"Content-Type": "text/javascript"});
+         res.write(data);
+         res.end();
+      });
+   } // TODO: Make it so I don't have to write literally every file I might use
 
 }).listen(3000);

a3cc25e50e8690fefaa47066ee1f2ac8a5f9b7b2
Pouchrat <gysoran@gmail.com>
2022-02-25 14:11:59 -0600

    Added default image, changed server file to allow for more filetypes

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
+// Thanks Mozilla
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Node_server_without_framework
+
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
-// Create server
-http.createServer(function (req, res){
-   // Main index
-   if (req.url == '/') {
-      fs.readFile('src/index.html', function(err, data){
-         res.writeHead(200, {'Content-Type': 'text/html'});
-         res.write(data);
-         res.end();
-      });
+http.createServer(function (request, response) {
+   console.log('request ', request.url);
+
+   var filePath = './src' + request.url;
+   if (/(\/)$/.test(filePath)) {
+        filePath += 'index.html';
+    }
+   else if (path.extname(filePath) == "") {
+      filePath += '/index.html';
    }
-   // Any other index, called as /
-   else if(/(\/)$/.test(req.url)) {
-      fs.readFile('src' + req.url + 'index.html', function(err, data){
-         res.writeHead(200, {'Content-Type': 'text/html'});
-         res.write(data);
-         res.end();
-      });
-   }
-   // Any html file
-   else if (/(\.html)$/.test(req.url)) {
-      fs.readFile('src' + req.url, function(err, data) {
-         res.writeHead(200, {"Content-Type": "text/html"});
-         res.write(data);
-         res.end();
-      });
-   }
-   // Any css file
-   else if(/(\.css)$/.test(req.url)) {
-      fs.readFile('src' + req.url, function(err, data){
-         res.writeHead(200, {"Content-Type": "text/css"});
-         res.write(data);
-         res.end();
-      });
-   }
-   // Any js file
-   else if(/(\.js)$/.test(req.url)) {
-      fs.readFile('src' + req.url, function(err, data) {
-          res.writeHead(200, {"Content-Type": "text/javascript"});
-          res.write(data);
-          res.end();
-      });
-   }
-   // Any url not ending in a file extension => parse as html
-   else if (!(/(\.\w+)/.test(req.url))) {
-      fs.readFile('src' + req.url + '/index.html', function(err, data){
-         res.writeHead(200, {'Content-Type': 'text/html'});
-         res.write(data);
-         res.end();
-      });
-   }
+   console.log("filePath " + filePath);
+
+   var extname = String(path.extname(filePath)).toLowerCase();
+   var mimeTypes = {
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+      '.json': 'application/json',
+      '.png': 'image/png',
+      '.jpg': 'image/jpg',
+      '.gif': 'image/gif',
+      '.svg': 'image/svg+xml',
+      '.wav': 'audio/wav',
+      '.mp4': 'video/mp4',
+      '.woff': 'application/font-woff',
+      '.ttf': 'application/font-ttf',
+      '.eot': 'application/vnd.ms-fontobject',
+      '.otf': 'application/font-otf',
+      '.wasm': 'application/wasm'
+   };
+
+   var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+   fs.readFile(filePath, function(error, content) {
+      if (error) {
+         if(error.code == 'ENOENT') {
+            fs.readFile('./404.html', function(error, content) {
+               response.writeHead(404, { 'Content-Type': 'text/html' });
+               response.end(content, 'utf-8');
+            });
+         }
+         else {
+            response.writeHead(500);
+            response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+         }
+      }
+      else {
+         response.writeHead(200, { 'Content-Type': contentType });
+         response.end(content, 'utf-8');
+      }
+   });
 
 }).listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
